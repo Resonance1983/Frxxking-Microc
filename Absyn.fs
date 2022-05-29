106a8fad35776565d8b4878eee012c74c8967d9f
章文韬 <729009583@qq.com>
2022-05-29 14:56:41 +0800

    添加type和语句

--- Absyn.fs
+++ Absyn.fs
@@ -13,6 +13,11 @@ module Absyn
 type typ =
   | TypI                             (* Type int                    *)
   | TypC                             (* Type char                   *)
+  | TypF                             (* Type float32                *)
+  | TypD                             (* Type double                 *)
+  | TypL                             (* Type long                   *)
+  | TypS                             (* Type string                 *)
+  | TypB                             (* Type boolean                *)
   | TypA of typ * int option         (* Array type                  *)
   | TypP of typ                      (* Pointer type                *)
 
@@ -25,8 +30,12 @@ and expr =                           // 表达式，右值
   | AssignPrim of string *access * expr      (* x+=e or  *p+=e or  a[e]+=e  *)
   | Addr of access                   (* &x   or  &*p   or  &a[e]    *)
   | CstI of int                      (* Constant                    *)
+  | CstF of float
+  | CstD of double
+  | CstS of string
   | Prim1 of string * expr           (* Unary primitive operator    *)
   | Prim2 of string * expr * expr    (* Binary primitive operator   *)
+  | Prim3 of expr * expr * expr      (*  ? ;                        *)
   | Andalso of expr * expr           (* Sequential and              *)
   | Orelse of expr * expr            (* Sequential or               *)
   | Call of string * expr list       (* Function call f(...)        *)
@@ -38,6 +47,12 @@ and access =                         //左值，存储的位置
 
 //基本语句定义                                                       
 and stmt =                                                         
+  | Switch of expr * stmt list
+  | Case of expr * stmt
+  | Default of stmt 
+  | DoUntil of stmt * expr
+  | DoWhile of stmt * expr
+  | ForIn of access * expr * expr * expr * stmt 
   | If of expr * stmt * stmt         (* Conditional                 *)
   | For of expr * expr * expr * stmt (* for loop                    *)
   | While of expr * stmt             (* While loop                  *)
@@ -48,12 +63,14 @@ and stmt =
 
 and stmtordec =                                                    
   | Dec of typ * string              (* Local variable declaration  *)
+  | DecAndAssign of typ * string * expr(*declaration and assign     *)
   | Stmt of stmt                     (* A statement                 *)
 
 // 顶级声明 可以是函数声明或变量声明
 and topdec = 
   | Fundec of typ option * string * (typ * string) list * stmt
   | Vardec of typ * string
+  | VardecAndAssign of typ * string * expr
 
 // 程序是顶级声明的列表
 and program = 
